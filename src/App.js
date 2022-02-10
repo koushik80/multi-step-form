972a184b115dc3bb3d6b94d48f51ab1af3390232
Koushik Dey <koushik.dey@live.com>
2022-02-11 01:33:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+import RegisterForm from "./components/RegisterForm";
+
+import "./App.css";
+
+function App() {
+    return ( <
+        RecoilRoot >
+        <
+        RegisterForm / >
+        <
+        /RecoilRoot>
+    );
+}
+
+export default App;
---
+++ src/components/RegisterForm/MobileNumber.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+    registerFormInputsState,
+    registerFormStepState,
+} from "../../atoms/registerForm";
+import { mobileNumberLength } from "../../selectors/registerForm";
+
+export default function MobileNumber() {
+    const [form, setForm] = useRecoilState(registerFormInputsState);
+    const [formStep, setFormStep] = useRecoilState(registerFormStepState);
+    const lengthOfMobileNumber = useRecoilValue(mobileNumberLength);
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        setFormStep("address");
+    };
+
+    return ( <
+            form onSubmit = { handleSubmit } >
+            <
+            input type = "text"
+            autoFocus placeholder = "Enter your mobile number (13 digits)"
+            onChange = {
+                (e) =>
+                setForm({
+                    ...form,
+                    mobileNumber: e.target.value,
+                })
+            }
+            value = { form.mobileNumber }
+            /> {
+            lengthOfMobileNumber === 13 ? ( <
+                p className = "success-message" > Mobile number is valid < /p>
+            ) : null
+        } <
+        button type = "submit"
+    disabled = { lengthOfMobileNumber == 13 ? false : true } >
+        Continue <
+        /button> <
+    button onClick = {
+            () => setFormStep("name")
+        } > Back < /button> < /
+        form >
+);
+}
